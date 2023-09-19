822de316282498c800ef769e72261e5dfa718fa2
Dali Selmi <75693386+mrdaliselmi@users.noreply.github.com>
2023-09-19 23:19:29 +0100

    Time: 382 ms (98.39%), Space: 0B (100%) - LeetHub

---
+++ 0176-second-highest-salary/0176-second-highest-salary.sql
@@ -0,0 +1,8 @@
+# Write your MySQL query statement below
+select (Select salary
+from Employee
+where salary <> (
+  select max(salary) 
+  from Employee )
+order by salary desc 
+limit 1) as SecondHighestSalary
