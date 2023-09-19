6b48599fc238292e43ea58003c8e57216a77c472
Dali Selmi <75693386+mrdaliselmi@users.noreply.github.com>
2023-09-19 23:08:17 +0100

    Time: 23 ms (88.33%), Space: 13.3 MB (50.85%) - LeetHub

---
+++ 0012-integer-to-roman/0012-integer-to-roman.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def intToRoman(self, num):
+        """
+        :type num: int
+        :rtype: str
+        """
+        units = ["","I","II","III","IV","V","VI","VII","VIII","IX"]
+        tens = ["","X","XX","XXX","XL","L","LX","LXX","LXXX","XC"]
+        hundreds = ["","C","CC","CCC","CD","D","DC","DCC","DCCC","CM"]
+        thousands = ["","M","MM","MMM"]
+        
+        return thousands[num//1000] + hundreds[(num%1000)//100] + tens[(num%100)//10] + units[num%10]
