a3a0d24fd1300f44459ec7e22372523a378988c5
Dali Selmi <75693386+mrdaliselmi@users.noreply.github.com>
2023-09-21 00:07:37 +0100

    Time: 7 ms (31.17%), Space: 10 MB (15.39%) - LeetHub

---
+++ 1580-shuffle-the-array/1580-shuffle-the-array.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    vector<int> shuffle(vector<int>& nums, int n) {
+        vector<int>ans(2*n,0);
+        int j=0;
+        for(int i=0;i<n;i++){
+            ans[j]=nums[i];
+            ans[j+1]=nums[i+n];
+            j+=2;
+        }
+        return ans;
+    }
+};
