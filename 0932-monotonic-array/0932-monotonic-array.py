f6ceedc90393b61d2ed3731362c0fd013b43b7d6
Dali Selmi <75693386+mrdaliselmi@users.noreply.github.com>
2023-09-29 17:18:11 +0100

    Time: 742 ms (82.67%), Space: 24.6 MB (32.58%) - LeetHub

--- 0932-monotonic-array/0932-monotonic-array.py
+++ 0932-monotonic-array/0932-monotonic-array.py
@@ -4,9 +4,8 @@ class Solution(object):
         :type nums: List[int]
         :rtype: bool
         """
-        n = len(nums)
         monotony = ''
-        for i in range(n-1):
+        for i in range(len(nums)-1):
             if nums[i]>nums[i+1]:
                 if monotony == '+':
                     return False
