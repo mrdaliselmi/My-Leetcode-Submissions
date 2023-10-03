dc7267de81d733b35465ff4d8c658822dc2e87b4
Dali Selmi <75693386+mrdaliselmi@users.noreply.github.com>
2023-10-03 13:41:24 +0100

    Time: 15 ms (71.05%), Space: 13.1 MB (74.31%) - LeetHub

---
+++ 1635-number-of-good-pairs/1635-number-of-good-pairs.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def numIdenticalPairs(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        pairs = 0
+        for i in range(len(nums)):
+            for j in range(i+1,len(nums)):
+                if nums[i]==nums[j]:
+                    pairs+=1
+        return pairs
