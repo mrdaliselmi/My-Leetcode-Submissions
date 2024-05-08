fb14d1cdac8ee261727e20ac27a705f31ccdeafd
Dali Selmi <75693386+mrdaliselmi@users.noreply.github.com>
2024-05-08 17:31:54 +0100

    Time: 50 ms (56.60%), Space: 13.2 MB (6.25%) - LeetHub

--- 0506-relative-ranks/0506-relative-ranks.py
+++ 0506-relative-ranks/0506-relative-ranks.py
@@ -6,15 +6,7 @@ class Solution(object):
         """
         score = [(i,value) for i,value in enumerate(score)]
         score.sort(key= lambda x:x[1], reverse=True)
-        for i in range(len(score)):
-            if i == 0:
-                score[i] = (score[i][0],score[i][1],"Gold Medal")
-            elif i == 1:
-                score[i] = (score[i][0],score[i][1],"Silver Medal")
-            elif i == 2:
-                score[i] = (score[i][0],score[i][1],"Bronze Medal")
-            else:
-                score[i] = (score[i][0],score[i][1],str(i+1))
+        score= [(score[i][0],"Gold Medal") if i==0 else (score[i][0],"Silver Medal") if i==1 else (score[i][0],"Bronze Medal") if i==2 else (score[i][0],str(i+1)) for i in range(len(score))]
         score.sort(key= lambda x:x[0])
-        return [x[2] for x in score]
+        return [x[1] for x in score]
         
