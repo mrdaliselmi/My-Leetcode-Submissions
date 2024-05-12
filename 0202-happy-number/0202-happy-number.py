ec12ea4fe952ec5a8fbbdb7c8f6c9dfdfaf9e872
Dali Selmi <75693386+mrdaliselmi@users.noreply.github.com>
2024-05-12 21:30:24 +0100

    Time: 17 ms (60.35%), Space: 11.7 MB (41.80%) - LeetHub

--- 0202-happy-number/0202-happy-number.py
+++ 0202-happy-number/0202-happy-number.py
@@ -10,8 +10,15 @@ class Solution(object):
                 res += (num%10)**2
                 num //= 10
             return res + num**2
-        while (n != 1 and n != 4):
+        # ----- MAFFS -----
+        # while (n != 1 and n != 4):
+        #     n = sumOfSquares(n)
+        # if n == 1:
+        #     return True
+        # return False
+        # ----- HashMap ----
+        visited = set()
+        while n != 1 and n not in visited:
+            visited.add(n)
             n = sumOfSquares(n)
-        if n == 1:
-            return True
-        return False
+        return n == 1
