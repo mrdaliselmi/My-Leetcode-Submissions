17b943192b8e42737deac754076348bcbe6c5e22
Dali Selmi <75693386+mrdaliselmi@users.noreply.github.com>
2025-03-26 20:34:56 +0100

    Time: 0 ms (100%), Space: 12.6 MB (12.7%) - LeetHub

---
+++ 1041-robot-bounded-in-circle/1041-robot-bounded-in-circle.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def isRobotBounded(self, instructions):
+        """
+        :type instructions: str
+        :rtype: bool
+        """
+        x, y, dx, dy = 0, 0, 0, 1
+        for i in instructions:
+            if i == 'R':
+                dx, dy = dy, -dx
+            if i == 'L':
+                dx, dy = -dy, dx
+            if i == 'G':
+                x, y = x + dx, y + dy
+        return (x,y) == (0,0) or (dx,dy) != (0,1)
+            
