23599bc4e2e6c1bbc47d6d70054afbcea571b642
Dali Selmi <75693386+mrdaliselmi@users.noreply.github.com>
2026-04-28 12:11:10 +0100

    Time: 10 ms (45.1%), Space: 16 MB (28.21%) - LeetHub

---
+++ 0207-course-schedule/0207-course-schedule.py
@@ -0,0 +1,32 @@
+from collections import defaultdict
+class Solution(object):
+    def canFinish(self, numCourses, prerequisites):
+        """
+        :type numCourses: int
+        :type prerequisites: List[List[int]]
+        :rtype: bool
+        """
+        graph = defaultdict(list)
+        for course, prereq in prerequisites:
+            graph[course].append(prereq)
+
+        visited = [0] * numCourses
+
+        def dfs(course):
+            if visited[course] == 1:
+                return False
+            if visited[course] == -1:
+                return True
+
+            visited[course] = 1
+            for prereq in graph[course]:
+                if not dfs(prereq):
+                    return False
+            visited[course] = -1
+            return True
+
+        for course in range(numCourses):
+            if not dfs(course):
+                return False
+
+        return True
